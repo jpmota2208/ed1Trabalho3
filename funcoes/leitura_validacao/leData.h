7260452f60c023ae63bf7a3842c8d43d1e38166a
marcelo046 <marc3lo046@gmail.com>
2018-05-16 22:20:51 -0300

    corrigido funcao de ler data

--- funcoes/leitura_validacao/leData.h
+++ funcoes/leitura_validacao/leData.h
@@ -1,56 +1,44 @@
 
 
 int dataValida(char *data) {
-  int dia=0, mes=0, ano=0;
-  static int chamadas = 0;
-  int tamanho = strlen(data);
-  char *letra;
+  int vData[] = {0, 0, 0}; // dia mes ano
+  int i, tamanho = strlen(data);
 
   if (tamanho != 10) {
     printf("\ninvalido! digite nesse formato (dd/mm/aaaa), ex: 01/07/1952\n");
     return 0;
   }
 
-  if (data[2] != '/' || data[5] != '/' ) {
-    printf("\ninvalido! digite nesse formato (dd/mm/aaaa), ex: 01/07/1952\n");
-    return 0;
-  }
+  data[2] = '1';
+  data[5] = '1';
 
-  data[2] = '0';
-  data[5] = '0';
-
-  for (letra = data; *letra; letra++) {
-    if (*letra < '0' || *letra > '9') {
-      printf("\ninvalido! digite apenas numeros! digite novamente: \n");
+  for ( i = 0; i < tamanho-1; i++) {
+    if (data[i] < '0' || data[i] > '9' ) {
+      printf("\ninvalido! digite apenas numeros .%c. i=%d\n",data[i],i);
       return 0;
     }
   }
+  vData[0] = (int)(data[0]-'0')*10 + (int)(data[1]-'0');
+  vData[1] = (int)(data[3]-'0')*10 + (int)(data[4]-'0');
+  vData[2] = (int)(data[6]-'0')*1000 + (int)(data[7]-'0')*100 + (int)(data[8]-'0')*10 + (int)(data[9]-'0');
 
-  //fflush(stdin);
-  //limpaBuffer();
+  if ((vData[0]*vData[1]*vData[2]) == 0) {
+    printf("\ninvalido! digite valores maiores que zero\n");
+    return 0;
+  } else if (vData[0] > 31 || vData[1] > 12 || vData[2] > 2017 || vData[2] < 1901) {
+    printf("\ninvalido! digite data valida\n");
+    return 0;
+  }
   data[2] = '/';
   data[5] = '/';
 
-  chamadas++;
   return 1;
 }
 
 void leData(char *data)  {
-  int i = 0;
   printf("Digite a data (dd/mm/aaaa)\nExemplos: 01/12/1998\t17/03/1988\n");
   do {
-    //limpaBuffer();
-    //fflush(stdin);
-    //__fpurge(stdin);
-    //scanf("%[^\n]s", data);
-    fgets(data,11,stdin);
+    scanf("%s", data);
     tiraTerminador(data);
-    //flush_in();
-    //if(i) limpaBuffer();
-    //if (data[0] == '\0')
-      //printf("ele eh \\0\n");
-
-    printf("\n#%d vc digitou {%s}\n",i, data );
-    i++;
   } while(!dataValida(data));
 }
--- funcoes/visuais/limpaTela.h
+++ funcoes/visuais/limpaTela.h
@@ -1,3 +1,3 @@
 void limpaTela() {
-  //system("clear");
+  system("clear");
 }
